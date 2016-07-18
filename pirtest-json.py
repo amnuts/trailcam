34a8f03c1730aa3321007d43d0f34c77852cf747
Andrew Collington <andy@amnuts.com>
2016-07-18 17:43:39 +0100

    Added pir test getting info from json config file.

---
+++ pirtest-json.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import time
+import json
+
+GPIO.setmode(GPIO.BCM)
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+GPIO.setup(config['pir'], GPIO.IN)
+
+try:
+	print "PIR Module Test (CTRL+C to exit)"
+	time.sleep(2)
+	print "Ready"
+	while True:
+		if GPIO.input(config['pir']):
+			print "%s %s" % (int(round(time.time() * 1000)), ": motion detected")
+
+except KeyboardInterrupt:
+	print "Quit"
+	GPIO.cleanup()
+
